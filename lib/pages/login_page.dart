e55987a36c499a0c6de842086fb9e320ffcd8650
vachspati <patiwhachas@gmail.com>
2025-02-04 00:54:53 +0530

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -38,7 +38,7 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children: [
                 Image.asset(
-                  "assets/images/hey.png",
+                  "assets/images/image.png",
                   fit: BoxFit.cover,
                 ),
                 SizedBox(
